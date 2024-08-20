fb7bdbeabe333c68655ef83ee2bc5261c042d525
Lorenzo Ciriaco <ciriacolorenzo5@gmail.com>
2024-08-20 18:22:45 -0300

    Fin del index, por ahora....

--- script.js
+++ script.js
@@ -16,3 +16,33 @@ cerrarMenu.addEventListener("click", () => {
     botonContactanos.classList.remove('button-contactanos-visible')
     nav.classList.remove("nav-visible")
 })
+
+function copyNumber(){
+    let number = document.querySelector(".phone");
+    let button = document.querySelector("#icon");
+    
+    navigator.clipboard.writeText(number.textContent);
+    button.classList.remove("bi-clipboard");
+    button.classList.add("bi-clipboard-check-fill");
+
+    // Cambia la clase de nuevo después de 2 segundos (2000 ms)
+    setTimeout(function() {
+        button.classList.remove("bi-clipboard-check-fill");
+        button.classList.add("bi-clipboard");
+    }, 2000);
+}
+
+function copyEmail(){
+    let email = document.querySelector(".email");
+    let button = document.querySelector("#iconEmail");
+    
+    navigator.clipboard.writeText(email.textContent);
+    button.classList.remove("bi-clipboard");
+    button.classList.add("bi-clipboard-check-fill");
+
+    // Cambia la clase de nuevo después de 2 segundos (2000 ms)
+    setTimeout(function() {
+        button.classList.remove("bi-clipboard-check-fill");
+        button.classList.add("bi-clipboard");
+    }, 2000);
+}
